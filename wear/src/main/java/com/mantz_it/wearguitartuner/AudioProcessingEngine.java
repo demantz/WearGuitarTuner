77fe86d4ce250a5b6d783fed9b7339735d2bd4df
Dennis Mantz <dennis.mantz@googlemail.com>
2015-01-08 16:24:15 +0100

    Initial commit

---
+++ wear/src/main/java/com/mantz_it/wearguitartuner/AudioProcessingEngine.java
@@ -0,0 +1,29 @@
+package com.mantz_it.wearguitartuner;
+
+/**
+ * <h1>RF Analyzer - Main Activity</h1>
+ *
+ * Module:      MainActivity.java
+ * Description: Main Activity of the RF Analyzer
+ *
+ * @author Dennis Mantz
+ *
+ * Copyright (C) 2014 Dennis Mantz
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+public class AudioProcessingEngine {
+}
